317a93d27da9f57a490b5208afdcc017adbaa29b
Nicodemus Kiplangat <nicodemusk84@gmail.com>
2024-06-27 09:15:06 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = "Nico Urs";
+let welcomeMessage = "Welcome to Flatbook, $(Nico Urs)!"
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, $(NICO URS)!"
+let shortGreeting = "Welcome, $(NICO URS)!";
